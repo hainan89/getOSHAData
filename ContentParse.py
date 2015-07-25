1c527c1c5a4b4c828481d3c6c7bdf8c521314352
hainan89 <hn.chen@live.com>
2015-07-26 01:26:56 +0800

    Modify the Mysql Operation
    1、删掉了mysql数据拼接操作被注释的代码
    2、对construction字段的处理没有进行json编码，只是对html标签进行了踢出，直接进行字符串存储
    
    3、对原始数据全部采用char或者text字段处理，简化了数据格式判断，数据结构分析被留到具体的数据分析阶段进行，本阶段只进行数据获取，最大可能保留数据的原始状态

--- ContentParse.py
+++ ContentParse.py
@@ -1,6 +1,7 @@
 from BrowserShadow import BrowserShadow
 from bs4 import BeautifulSoup
-import mysql.connector
+
+import mysql.connector
 import sys, os
 
 
@@ -81,8 +82,6 @@ def get_record_list(SICNo, s_d, s_m, s_y, e_d, e_m, e_y):
         # prase the specified record
         """collect the records into mysql database"""
         parse_page_obtian_event(page_soup)
-        break
-
 
         checked_num = checked_num + p_show
         #print(checked_num)
@@ -126,9 +125,6 @@ def parse_page_obtian_event(page_soup):
         abstract_info = {'SummaryNr':SummaryNr , 'EventDate':EventDate, 'ReportID':ReportID, 'Fat':str(Fat), 'SIC':SIC, 'EventDesc':EventDesc}
         parse_accident_details(page_soup_d , abstract_info)
 
-        break
-
-
         
 def parse_accident_details(page_soup_d, abstract_info):
 
@@ -158,29 +154,6 @@ def parse_accident_details(page_soup_d, abstract_info):
     NonBldgHt = event_content_details[keyword_position + 2].find_all('td')[4].get_text()
 
     # insert the case information
-    """
-    cases_info_str = 'SummaryNr, EventDate, ReportID, Fat, SIC, EventDesc, '
-    cases_info_str = cases_info_str + 'EventDescD, Keywords, EndUse, '
-    cases_info_str = cases_info_str + 'ProjType, ProjCost, Stories, NonbldgHt'
-
-    cases_info_value_str = "'" + str(abstract_info['SummaryNr']) + "'" + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(abstract_info['EventDate']) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(abstract_info['ReportID']) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + str(abstract_info['Fat']) + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(abstract_info['SIC']) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(abstract_info['EventDesc']) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(EventDescD) + "'"  + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(Keyworkds) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(EndUse) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(ProjType) + "'"   + ','
-    cases_info_value_str = cases_info_value_str + "'" + str(ProjCost) + "'" + ','
-    cases_info_value_str = cases_info_value_str + str(Stories) + ','
-    cases_info_value_str = cases_info_value_str + str(NonBldgHt)
-    
-    insert_case_info_sql_str = "insert into cases_info({case_info}) values ({case_info_value})"
-    insert_case_info_sql_str = insert_case_info_sql_str.format(case_info = cases_info_str, case_info_value = cases_info_value_str)
-    """
-
     insert_sql_str = ("insert into cases_info "
                       "(SummaryNr, EventDate, ReportID, Fat, SIC, EventDesc, "
                       "EventDescD, Keywords, EndUse, ProjType, ProjCost, Stories, NonbldgHt)"
@@ -190,7 +163,7 @@ def parse_accident_details(page_soup_d, abstract_info):
                      EventDescD, Keyworkds, EndUse, ProjType, ProjCost, Stories, NonBldgHt)
     
     #print(insert_case_info_sql_str)
-    """
+
     try:
         cursor.execute(insert_sql_str, insert_values)
         print('=============================')
@@ -201,7 +174,7 @@ def parse_accident_details(page_soup_d, abstract_info):
         print("Insert cases_info Table Failed.")
         print("Error: {}".format(err.msg))
         sys.exit()
-    """
+
     
     for index in range(2, keyword_position - 1, 2):
         current_insp_id = event_content_details[index].find_all('td')[0].get_text()
@@ -211,27 +184,12 @@ def parse_accident_details(page_soup_d, abstract_info):
         #print(current_insp_id + '--' + current_insp_date + '--' + current_insp_sic + '--' + current_insp_establishment_name)
 
         # insert into table inspection_info
-        """
-        insp_info_str = 'InspectionID, OpenDate, SIC, EstablishmentName'
-
-        insp_info_value_str = "'" + str(current_insp_id) + "'" + ','
-        insp_info_value_str = insp_info_value_str + "'" + str(current_insp_date) + "'" + ','
-        insp_info_value_str = insp_info_value_str + "'" + str(current_insp_sic) + "'" + ','
-        insp_info_value_str = insp_info_value_str + "'" + str(current_insp_establishment_name) + "'"
-
-        insert_insp_info_sql_str = "insert into inspection_info({insp_info}) values ({insp_info_value})"
-        insert_insp_info_sql_str = insert_insp_info_sql_str.format(insp_info = insp_info_str, insp_info_value = insp_info_value_str)
-
-        print(insert_insp_info_sql_str)
-        """
-
         insert_sql_str = ("insert into inspection_info "
                       "(InspectionID, OpenDate, SIC, EstablishmentName)"
                       "values(%s, %s, %s, %s)")
 
         insert_values = (current_insp_id, current_insp_date, current_insp_sic,current_insp_establishment_name )
 
-        """
         try:
             cursor.execute(insert_sql_str, insert_values)
             print('=============================')
@@ -242,8 +200,6 @@ def parse_accident_details(page_soup_d, abstract_info):
             print("Insert inspection_info Table Failed.")
             print("Error: {}".format(err.msg))
             sys.exit()
-        break
-        """
 
     for index in range(keyword_position + 4, len(event_content_details)):
         current_employee_eid = event_content_details[index].find_all('td')[0].get_text()
@@ -261,38 +217,23 @@ def parse_accident_details(page_soup_d, abstract_info):
         construction_str = construction_str.replace('</b>' , '')
         construction_str = construction_str.replace('</br>' , '')
         construction_list = construction_str.split('<br>')
+
+        """
         current_employee_consruction = "{"
         for index in range(0, len(construction_list) - 1):
             item_one_name = construction_list[index].split(':')
             current_employee_consruction = current_employee_consruction + "'"+item_one_name[0]+"':'"+item_one_name[1] + "',"
         item_one_name = construction_list[index + 1].split(':')
         current_employee_consruction =current_employee_consruction + "'"+item_one_name[0]+"':'"+item_one_name[1] + "'}"
+        """
         #print(current_employee_eid + '--' + current_employee_insp_id + '--' + current_employee_age + '--' + current_employee_sex + '--' + current_employee_degree + '--' + current_employee_nature + '--' + current_employee_occupation + '--' + current_employee_consruction)
-
-        """
-        employee_info_str = 'EID, SummaryNr, InspectionID, Age, Sex, Degree, Nature, Occupation, Construction'
-        employee_info_value_str = str(current_employee_eid) + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(abstract_info['SummaryNr']) + "'" + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(current_employee_insp_id) + "'" + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(current_employee_age) + "'" + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(current_employee_sex) + "'" + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(current_employee_degree) + "'" + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(current_employee_nature) + "'" + ','
-        employee_info_value_str = employee_info_value_str + "'" + str(current_employee_occupation) + "'" + ','
-        employee_info_value_str = employee_info_value_str + '"' + str(current_employee_consruction) + '"'
-
-        insert_employee_info_sql_str = "insert into case_employees({emp_info}) values ({emp_info_value})"
-        insert_employee_info_sql_str = insert_employee_info_sql_str.format(emp_info = employee_info_str, emp_info_value = employee_info_value_str)
-
-        print(insert_employee_info_sql_str)
-        """
 
         insert_sql_str = ("insert into case_employees "
                       "(EID, SummaryNr, InspectionID, Age, Sex, Degree, Nature, Occupation, Construction)"
                       "values(%s, %s, %s, %s, %s, %s, %s, %s, %s)")
 
         insert_values = (current_employee_eid, abstract_info['SummaryNr'], current_employee_insp_id,current_employee_age,
-                         current_employee_sex, current_employee_degree, current_employee_nature, current_employee_occupation, current_employee_consruction)
+                         current_employee_sex, current_employee_degree, current_employee_nature, current_employee_occupation, str(construction_list))
         
         try:
             cursor.execute(insert_sql_str, insert_values)
@@ -304,13 +245,12 @@ def parse_accident_details(page_soup_d, abstract_info):
             print("Insert case_employees Table Failed.")
             print("Error: {}".format(err.msg))
             sys.exit()
-        break
             
     cursor.close()
     cnx.close()
     
     
 
-get_record_list(15, '15', '06', '1989', '15', '06', '2015')
+get_record_list(15, '26', '07', '2005', '26', '07', '2015')
 #article_info_list = get_articles_list(15, 0 , 0)
 #get_article_content(article_info_list)
